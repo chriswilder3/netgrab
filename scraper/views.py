bc5b82407964e0a5a2ad9730d9cf1c8935a7aa2f
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-06 17:20:55 +0530

    styled and added tables for link name, adress

--- scraper/views.py
+++ scraper/views.py
@@ -3,16 +3,25 @@ from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
 
+from .models import LinkAdress
+
 # Create your views here.
 
 def scrape( request):
     url = 'https://www.google.com'
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
-    links = []
+    
 
     for link in soup.find_all('a'):
-        links.append(link.get('href'))
-    
+        link_address = link.get('href') # Gets the link attr of a tags
+        link_name = link.string # Content inside a tag, which is usually
+                                # the name of that link in most sites
+        link = LinkAdress( name = link_name, address = link_address)
+        link.save()
+        # We can also use LinkAdress.create( field =val...)
+        
+    links = LinkAdress.objects.all()
+
     return render(request, 'index.html', {'links': links})
 
