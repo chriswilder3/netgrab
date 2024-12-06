c116b2037cb6ffd5867ad2344f338f86ed2dba1c
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-06 16:09:02 +0530

    created index page and scrape view

--- scraper/views.py
+++ scraper/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from bs4 import BeautifulSoup
+import requests
+
 # Create your views here.
 
+def scrape( request):
+    url = 'https://www.google.com'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    links = []
+
+    for link in soup.find_all('a'):
+        links.append(link.get('href'))
+    
+    return render(request, 'index.html', {'links': links})
 
