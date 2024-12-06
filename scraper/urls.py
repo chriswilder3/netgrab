105dfdb9875d2970a1983e3b68112fe44c6f9884
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-06 17:49:54 +0530

    working , able to take input

--- scraper/urls.py
+++ scraper/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.scrape, name='index')
+    path('', views.scrape, name='index'),
+    path('delete', views.delete, name='delete'),
 ]
--- scraper/views.py
+++ scraper/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from bs4 import BeautifulSoup
 import requests
@@ -8,20 +8,33 @@ from .models import LinkAdress
 # Create your views here.
 
 def scrape( request):
-    url = 'https://www.google.com'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    
+    # url = request.POST.get('url')
 
-    for link in soup.find_all('a'):
-        link_address = link.get('href') # Gets the link attr of a tags
-        link_name = link.string # Content inside a tag, which is usually
-                                # the name of that link in most sites
-        link = LinkAdress( name = link_name, address = link_address)
-        link.save()
-        # We can also use LinkAdress.create( field =val...)
+    if request.method == 'POST':
+        url = request.POST.get('url', '') 
+            # Either get url if available or assign '' to url
+
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
         
+        for link in soup.find_all('a'):
+            link_address = link.get('href') # Gets the link attr of a tags
+            link_name = link.string # Content inside a tag, which is usually
+                                    # the name of that link in most sites
+            link = LinkAdress( name = link_name, address = link_address)
+            link.save()
+            # We can also use LinkAdress.create( field =val...)
+
+        return redirect('/')
+
+    # If its get request
     links = LinkAdress.objects.all()
 
     return render(request, 'index.html', {'links': links})
+
+def delete(request):
+    LinkAdress.objects.all().delete()
+
+    return render(request, 'index.html')
+
 
